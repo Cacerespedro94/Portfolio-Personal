0ed244230af9144e85c57d909ca120a6c013ccd3
Cacerespedro94 <cacerespedro94@gmail.com>
2020-05-08 01:12:46 -0300

    Se agregaron funciones JS

--- Js/Js.js
+++ Js/Js.js
@@ -7,11 +7,31 @@ window.addEventListener('load',function(){
     var texto = document.getElementById("ImputTexto");
     var nombre = document.getElementById("ImputNombre");
 
-        if(email.textContent =="")
+        if(email.value =="")
         {
             var error = document.getElementById("EmailError");
-            error.innerHTML = "Tienes que ingresar un email..."
+            error.innerHTML = "Tienes que ingresar un email...";
+        }
+        else{
+            document.querySelector("#EmailError").style.display = "none";
         }
 
+        if(texto.value == "")
+        {
+            var error = document.getElementById("TextoError");
+            error.innerHTML = "Tienes que ingresar un comentario...";
+        }
+        else{
+            document.querySelector("#TextoError").style.display = "none";
+        }
+        if(nombre.value == "")
+        {
+            var error = document.getElementById("NombreError");
+            error.innerHTML = "Tienes que ingresar tu nombre...";
+        }
+        else{
+            document.querySelector("#NombreError").style.display = "none";
+        }
+        
 });
 });
